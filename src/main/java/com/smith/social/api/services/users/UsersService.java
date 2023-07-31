8d5465a813708f90378602ba6a47db4c7c1e5e9d
j-m-smith426 <joabsmith4226@gmail.com>
2023-07-31 00:04:47 -0400

    Basic testing setup

--- src/main/java/com/smith/social/api/services/users/UsersService.java
+++ src/main/java/com/smith/social/api/services/users/UsersService.java
@@ -1,8 +1,22 @@
 package com.smith.social.api.services.users;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.smith.social.api.entities.Users;
+import com.smith.social.api.repository.UsersRepo;
 
 @Service
 public class UsersService {
+
+    @Autowired
+    private UsersRepo usersRepo;
+
+    public List<Users> getAllUsers() {
+        List<Users> data = usersRepo.findAll();
+        return null;
+    }
     
 }
